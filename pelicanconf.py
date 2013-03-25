c231dc5e28624e6d88b67185b9cb9844a7b2ba3a
Greg Bowyer <gbowyer@shopzilla.com>
2013-03-25 00:22:27 -0700

    Add Theme stuff

--- pelicanconf.py
+++ pelicanconf.py
@@ -3,11 +3,26 @@
 
 AUTHOR = u'Greg Bowyer'
 SITENAME = u'bonsaichicken.org'
-SITEURL = ''
+SITEURL = 'http://bonsaichicken.org'
+FEED_DOMAIN = 'http://bonsaichicken.org'
 
 TIMEZONE = 'America/Los_Angeles'
 
 DEFAULT_LANG = u'en'
+
+DEFAULT_CATEGORY = u'Stuff'
+DATE_FORMAT = { 'en': '%d %m %Y' }
+
+THEME = "./themes/livingston/"
+
+#Navigation sections and relative URL:
+SECTIONS = [('Blog', 'index.html'),
+        ('Archive', 'archives.html'),
+        ('Tags', 'tags.html'),
+        ('Projects', 'pages/projects.html'),
+        ('About', 'pages/about-me.html')]
+
+NUM_FULL_ARTICLES = 10
 
 # Blogroll
 LINKS =  (('Pelican', 'http://docs.notmyidea.org/alexis/pelican/'),
@@ -20,3 +35,14 @@ SOCIAL = (('You can add links in your config file', '#'),
           ('Another social link', '#'),)
 
 DEFAULT_PAGINATION = 10
+# Social widget
+SOCIAL = (('You can add links in your config file', '#'),
+          ('Another social link', '#'),)
+
+DEFAULT_PAGINATION = 10
+
+DISQUS_SITENAME = "bonsaichicken"
+TWITTER_USERNAME = 'bowyakka'
+#LINKEDIN_URL = 'http://en.linkedin.com/in//en'
+GITHUB_URL = 'http://github.com/GregBowyer'
+GOOGLE_ANALYTICS = "UA-25131621-1"
